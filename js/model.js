de9f360441cb4f1a4d9817c3bd3717f6033a881d
kotony1016 <ko3chutony831016@gmail.com>
2021-06-26 14:48:08 +0800

    add

--- js/model.js
+++ js/model.js
@@ -1,11 +1,9 @@
-// ---------------漢堡選單收合--------------//
+
 
 
 // ---------------子選單收合----------------//
-var product = document.getElementById('product');
-var ol = document.getElementById('sub_menu');
-product.addEventListener('click',function(){
-
-    if(ol.style.max-height==0 px){ol.style.max-height==210 px}
-    else{ol.style.max-height ==0 px}
+$(function(){
+    $('#product').click(function(){
+        $('#sub_menu').slideToggle(1000,'linear');
+    });
 });
